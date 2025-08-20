db4453113ba4ac16691a24a699efab2a1384e03a
ShelkePravin <pravinshelke844@gmail.com>
2025-08-20 23:34:08 +0530

    Initial commit

---
+++ API/API/EmailService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace API
+{
+    public class EmailService
+    {
+        public EmailService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void SendEmail(string toEmail, string subject, string body)
+        {
+            var fromEmail = Configuration["Constants:FromEmail"] ?? string.Empty;
+            var fromEmailPassword = Configuration["Constants:EmailAccountPassword"] ?? string.Empty;
+
+            using (var message = new MailMessage())
+            {
+                message.From = new MailAddress(fromEmail);
+                message.Subject = subject;
+                message.Body = body;
+                message.IsBodyHtml = true;
+                message.To.Add(toEmail);
+
+                using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtpClient.Credentials = new NetworkCredential(fromEmail, fromEmailPassword);
+                    smtpClient.EnableSsl = true;
+                    smtpClient.Send(message);
+                }
+            }
+        }
+    }
+}
---
+++ API/API/Migrations/20250820173902_initial2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2025, 8, 20, 13, 28, 12, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2023, 11, 1, 13, 28, 12, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
